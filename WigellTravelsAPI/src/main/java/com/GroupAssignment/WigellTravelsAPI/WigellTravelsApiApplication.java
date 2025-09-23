450f41f53e6fa758fb39a1acfe35f2615e3b5dfe
samuel.funck <159692435+samuelFNK@users.noreply.github.com>
2025-09-23 12:58:08 +0200

    Initial commit

---
+++ WigellTravelsAPI/src/main/java/com/GroupAssignment/WigellTravelsAPI/WigellTravelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.GroupAssignment.WigellTravelsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellTravelsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellTravelsApiApplication.class, args);
+	}
+
+}
---
+++ WigellTravelsAPI/src/test/java/com/GroupAssignment/WigellTravelsAPI/WigellTravelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupAssignment.WigellTravelsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellTravelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
